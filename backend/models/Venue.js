fd403f1a671eb86fd8bbb616eae53682fbe86935
MualusiLilimu <lilimumualusi37@gmail.com>
2025-09-21 14:47:15 +0200

    do not touch this

--- backend/models/Venue.js
+++ backend/models/Venue.js
@@ -12,7 +12,12 @@ const VenueSchema = new mongoose.Schema({
     image: {
         url: String,
         public_id: String
+    },
+    map: {
+        url: String,
+        public_id: String
     }
+
 });
 
 module.exports = mongoose.model("Venue", VenueSchema);
