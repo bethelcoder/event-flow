10b3510bdbe2fc2ab056a4777d5f9ca5ea0f7dbe
MualusiLilimu <lilimumualusi37@gmail.com>
2025-09-24 21:52:33 +0200

    handled guest invite cases

--- backend/controllers/guestsController.js
+++ backend/controllers/guestsController.js
@@ -11,32 +11,36 @@ exports.registerGuest = async (req, res) => {
 
     const managerId = req.body.manager.id;
     const event = await Event.findOne({ 'organizer.id': managerId },{ _id: 1 }).lean();
-    const eventId = event?._id;
-    // Generate reference number
-    const refNumber = generateRefNumber("Event Flow", "Techno AI Conference - 2025 ");
-    // Create encrypted QR payload
-    const payload = { email, eventId, refNumber };
-    const encryptedQR = encryptData(payload);//info encrypted by the qrCode
+    if(event){
+      const eventId = event?._id;
+      // Generate reference number
+      const refNumber = generateRefNumber("Event Flow", "Techno AI Conference - 2025 ");
+      // Create encrypted QR payload
+      const payload = { email, eventId, refNumber };
+      const encryptedQR = encryptData(payload);//info encrypted by the qrCode
 
-    // Generate QR image
-    const qrCodeUrl = await QRCode.toDataURL(encryptedQR);
-    const existingGuest = await Guest.findOne({ email });
-    if(existingGuest) return res.status(400).json({ message: "Guest already exists"});
+      // Generate QR image
+      const qrCodeUrl = await QRCode.toDataURL(encryptedQR);
+      const existingGuest = await Guest.findOne({ email });
+      if(existingGuest) return res.status(400).json({ message: "Guest already exists"});
+          const guest = await Guest.create({
+          email,
+          fullName,
+          eventId,
+          qrCode: encryptedQR,
+          qrCodeUrl,
+          refNumber
+      });
+      await guest.save();
+      // 📩 Send email with QR code
+      await sendGuestQRCode(email, fullName, refNumber, qrCodeUrl);
+      res.redirect("/manager/home");
+    }
 
-    const guest = await Guest.create({
-      email,
-      fullName,
-      eventId,
-      qrCode: encryptedQR,
-      qrCodeUrl,
-      refNumber
-    });
-    await guest.save();
+    else{
+      res.status(400).json({message: "Create an event first"});
+    }
 
-    // 📩 Send email with QR code
-    await sendGuestQRCode(email, fullName, refNumber, qrCodeUrl);
-
-    res.redirect("/manager/home");
   } catch (err) {
     console.error(err);
     res.status(500).json({ success: false, error: err.message });
